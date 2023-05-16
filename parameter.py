3df70a40f538818c2520a96aa39755db3a8b0ede
Grudon <grudonfactory@gmail.com>
2023-05-16 16:03:10 +0200

    Removed private information

--- parameter.py
+++ parameter.py
@@ -1,8 +1,14 @@
 #Parameters
+#Please add your device's Primary Connection String from IoT Hub
 
 data = {
-    "CONNECTION_STRING" : "HostName=IoT-Hub-Tobias1.azure-devices.net;DeviceId=raspberry_bridge;SharedAccessKey=DedCWlDQKFwHFtChf3feKSaA7xsZ7TEpondoHaWQbaY=",
+    "CONNECTION_STRING" : "YOUR_PRIMARY_CONNECTION_STRING",
     "BROKER" : "192.168.0.10",
-    "SUB_TOPICS" : ["f/i/order","f/i/stock", "f/i/state/hbw", "f/i/state/mpo", "f/i/state/vgr", "f/i/state/sld", "f/i/state/dsi", "f/i/state/dso"],
+    "SUB_TOPICS" : ["f/i/order",
+        "f/i/stock", "f/i/state/hbw", 
+        "f/i/state/mpo", "f/i/state/vgr", 
+        "f/i/state/sld", 
+        "f/i/state/dsi", 
+        "f/i/state/dso"],
     "PUB_TOPIC" : "f/o/order",
 }
